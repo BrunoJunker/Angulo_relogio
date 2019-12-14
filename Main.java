8ff49caf20e82a6739c69050b59b28435c5d6a52
Bruno Henrique De Mello Junker <37717467+BrunoJunker@users.noreply.github.com>
2019-12-14 13:32:26 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Favor informar a hora entre 0 e 12");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a hora:");
+        int hora = sc.nextInt();
+
+        System.out.print("Digite o minuto:");
+        int minuto = sc.nextInt();
+
+        if (hora > 12 || minuto > 59) {
+            System.out.println("Entrada invalida!");
+            return;
+        }
+
+        int posicaoMinuto = obterPosicaoMinuto(minuto);
+        int posicaoHora = obterPosicaoHora(hora);
+
+        int diferenca = objterDiferenca(posicaoMinuto, posicaoHora);
+
+        System.out.println("A diferenca dos angulo entre os 2 ponteiros do relogio = " + diferenca + " graus");
+
+    }
+
+    public static int obterPosicaoMinuto(int minutos) {
+        // 360 = volta completa, dividimos por 60 pra saber quando cada minuto representa em graus
+        return minutos * (360 / 60);
+    }
+
+    public static int obterPosicaoHora(int horas) {
+        // se hora for igual a 12 retorna 0 se nao
+        // 360 = volta completa, dividimos por 12 pra saber quando cada hora representa em graus
+        return horas == 12 ? 0 : horas * (360 / 12);
+    }
+
+    private static int objterDiferenca(int posicaoMinuto, int posicaoHora) {
+        int diferenca = 0;
+        // verifica qual é o maior grau, para fazer a subtracao nao dar negativa
+        if (posicaoHora > posicaoMinuto) {
+            diferenca = posicaoHora - posicaoMinuto;
+        } else {
+            diferenca = posicaoMinuto - posicaoHora;
+        }
+        return diferenca;
+    }
+
+}
